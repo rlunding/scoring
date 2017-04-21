f4b38ce072269f49fa705e57a214882667cf71b1
MRingive <mathiasringive@hotmail.com>
2017-04-21 10:45:53 +0200

    Test

--- cli/commands/cmd_adhoc.py
+++ cli/commands/cmd_adhoc.py
@@ -1,4 +1,5 @@
 from subprocess import call
+import requests
 
 import click
 
@@ -11,4 +12,5 @@ def cli():
     :return: str
     """
 
-    return click.echo(call(["ping", "-c", "5", "192.168.0.101"]))
+    #return click.echo(call(["ping", "-c", "5", "192.168.0.101"]))
+    return click.echo(requests.get('192.168.4.2:5000/scores').content)
