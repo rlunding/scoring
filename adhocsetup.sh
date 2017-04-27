854d2044c7965b344dbc78d33830d132c02fb796
MRingive <mathiasringive@hotmail.com>
2017-04-27 13:43:38 +0200

    New adhoc setup script using ports in adhoc.txt file

--- adhocsetup.sh
+++ adhocsetup.sh
@@ -6,7 +6,9 @@ filename='/home/pi/scoring/adhoc.txt'
 
 while read p; do
     echo $p
-    sudo arp -s $p
+    a=( $p )
+    #echo ${a[0]} ${a[2]}
+    sudo arp -s ${a[0]} ${a[2]}
 done < $filename
 
 exit 0
