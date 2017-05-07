5af5e90e01db4b079da7674f9bbbee1cd1eed75e
lunding <lunding@me.com>
2017-05-07 15:30:09 +0200

    Cosmetic changes.

--- scoring/static/scripts/match.js
+++ scoring/static/scripts/match.js
@@ -58,6 +58,20 @@ var add_buttons_to_score_fields = function () {
     if (score_field_2.length) {
         add_buttons(score_field_2);
     }
+
+    var start_button = $('<input class="btn btn-lg btn-default" type="button" id="now_start" value="Now"/>');
+    $('#start_date').parent().append(start_button);
+    $('#now_start').click(function () {
+        $('#start_date').data('DateTimePicker').date(new Date());
+    });
+
+    var end_button = $('<input class="btn btn-lg btn-default" type="button" id="now_end" value="Now"/>');
+    $('#end_date').parent().append(end_button);
+    $('#now_end').click(function () {
+        $('#end_date').data('DateTimePicker').date(new Date());
+    });
+
+    $('#submit').addClass('btn-lg');
 };
 
 var add_buttons = function (score_field) {
@@ -68,7 +82,7 @@ var add_buttons = function (score_field) {
             var value = values[i];
             var sign = value > 0 ? '+' : '';
             var id = score_field.attr('id') + '_button_' + values[i];
-            var button = $('<input type="button" id="'+id+'" value="'+sign + value+'"/>');
+            var button = $('<input class="btn btn-lg btn-default" type="button" id="'+id+'" value="'+sign + value+'"/>');
             var div = score_field.parent();
             div.append(button);
 
