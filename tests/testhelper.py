9fe48ecb60e7c65a723870ac5c1beb9f51e8aa46
MRingive <mathiasringive@hotmail.com>
2017-05-13 17:44:28 +0200

    Experiment distributions

--- tests/testhelper.py
+++ tests/testhelper.py
@@ -3,17 +3,24 @@ import dateutil.parser
 import json
 from pprint import pprint
 
-with open('push_test/push_test_at_44.txt') as data_file:
+file_to_open = 'push_test/push_test_at_44.txt'
+file_to_write = 'push_test/push_test_diff_distribution.txt'
+
+with open(file_to_open) as data_file:
     data = json.load(data_file)
 
-time_array = []
+time_array = [0] * 30
 for log in data['logs']:
     time_receiver = dateutil.parser.parse(log['timestamp_receiver'])
     time_sender = dateutil.parser.parse(log['timestamp_sender'])
     time_diff = time_receiver - time_sender
-    time_array.append(time_diff.total_seconds())
+    time_diff = int(time_diff.total_seconds())
+    #time_array.append(time_diff.total_seconds())
 
-with open('push_test/push_test_diff_time.txt', "w+") as write_file:
+    time_array[time_diff] = time_array[time_diff] + 1
+
+
+with open(file_to_write, "w+") as write_file:
     for item in time_array:
         write_file.write("%s\n" % item)
 
